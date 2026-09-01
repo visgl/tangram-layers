83252618fefd9cc461b5b9c00f7be4e15bcdaa70
Ib Green <ib@unfolded.ai>
2026-09-01 10:23:15 -0400

    fix: normalize source test license headers

--- types/geojson-vt.d.ts
+++ types/geojson-vt.d.ts
@@ -1,6 +1,6 @@
 // tangram-layers
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2026 vis.gl contributors
+// Copyright (c) vis.gl contributors
 
 declare module 'geojson-vt' {
     type GeoJsonTile = {features?: any[]};
